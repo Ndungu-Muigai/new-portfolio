2e7c7ad77f87d1861971d28d414e5591df107284
Samuel Muigai <ndungu.muigai01@gmail.com>
2024-06-09 16:04:00 +0300

    Modified the styling of the links to only target nav links

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,10 +4,10 @@ const Navbar = () =>
 {
     return ( 
         <nav className="bg-slate-600 text-white flex flex-row p-5 text-base space-x-6 border-red-400 w-full fixed top-0 md:text-lg justify-end z-10">
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/projects">Projects</Link>
-            <Link to="/contacts">Contacts</Link>
+            <Link to="/" className="nav-links">Home</Link>
+            <Link to="/about" className="nav-links">About</Link>
+            <Link to="/projects" className="nav-links">Projects</Link>
+            <Link to="/contacts" className="nav-links">Contacts</Link>
         </nav>
      );
 }
