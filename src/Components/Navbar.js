a60b3754cbfc337774364222d1c00155d243d146
Samuel Muigai <ndungu.muigai01@gmail.com>
2024-06-09 15:58:43 +0300

    Created and styled the projects section and added a z-index to the navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => 
 {
     return ( 
-        <nav className="bg-slate-600 text-white flex flex-row p-5 text-base space-x-3 border-red-400 w-full fixed top-0 md:text-lg justify-end z-10">
+        <nav className="bg-slate-600 text-white flex flex-row p-5 text-base space-x-6 border-red-400 w-full fixed top-0 md:text-lg justify-end z-10">
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
             <Link to="/projects">Projects</Link>
