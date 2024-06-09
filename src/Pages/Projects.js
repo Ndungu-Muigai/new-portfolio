f0eb7059e1dd66191b9bdd84ada3e46f9c38883f
Samuel Muigai <ndungu.muigai01@gmail.com>
2024-06-09 15:53:27 +0300

    Created and styled the projects section and added a z-index to the navbar

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -15,12 +15,8 @@ const Projects = () =>
                         <p className="text-gray-700 text-base">{description}</p>
                     </div>
                     <div className="px-6 py-4 flex justify-between">
-                        <Link to={demo_link} className="bg-blue-500 hover:bg-blue-700 text-white -font-bold py-2 px-4 rounded mr-2">
-                            Live Demo
-                        </Link>
-                        <Link to={source_code} className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-                            Source Code
-                        </Link>
+                        <Link to={demo_link} target="_blank" className="bg-blue-500 hover:bg-blue-700 text-white -font-bold py-2 px-4 rounded mr-2">Live Demo</Link>
+                        <Link to={source_code} target="_blank" className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">Source Code</Link>
                     </div>
                 </div>
             )
