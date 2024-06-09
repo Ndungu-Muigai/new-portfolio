83f46762e365e5ca09b46c8cfbbac94d6089d1a3
Samuel Muigai <ndungu.muigai01@gmail.com>
2024-06-09 13:37:29 +0300

    Added links to the social media icons

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,30 +10,30 @@ const Footer = () => {
     const year = date.getFullYear();
 
     return (
-        <footer className="fixed bottom-0 inset-x-0 flex flex-col items-center bg-white">
+        <footer className="fixed bottom-0 inset-x-0 flex flex-col items-center bg-white border-t-2">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-5 w-full max-w-4xl mx-auto p-6">
                 <div className="flex flex-col mx-4 md:mx-10 space-y-3">
-                    <h1 className="uppercase text-cyan-500">Quick links</h1>
+                    <h1>Quick links</h1>
                     <Link to={"/"}>Home</Link>
                     <Link to={"/about"}>About</Link>
                     <Link to={"/projects"}>Projects</Link>
                     <Link to={"/contacts"}>Contacts</Link>
                 </div>
                 <div className="flex flex-col mx-4 md:mx-10 space-y-3">
-                    <h1 className="uppercase text-cyan-500">Find me on social media</h1>
+                    <h1>Find me on social media</h1>
                     <div className="flex flex-wrap mt-4 space-x-4 md:space-x-18">
-                        <Link className="icon"><FaGithub /></Link>
-                        <Link className="icon"><FaLinkedinIn /></Link>
-                        <Link className="icon"><FaXTwitter /></Link>
-                        <Link className="icon"><FaInstagram /></Link>
-                        <Link className="icon"><FaWhatsapp /></Link>
-                        <Link className="icon"><FaPhoneVolume /></Link>
-                        <Link className="icon"><IoMailOutline /></Link>
+                        <Link className="icon" to={"https://www.github.com/ndungu-muigai"} target="_blank" title="GitHub"><FaGithub /></Link>
+                        <Link className="icon" to={"https://www.linkedin.com/in/samuel-muigai-560355224/"} target="_blank" title="LinkedIn"><FaLinkedinIn /></Link>
+                        <Link className="icon" to={"https://www.twitter.com/MzeeMuigai"} target="_blank" title="X"><FaXTwitter /></Link>
+                        <Link className="icon" to={"https://www.instagram.com/mr.muigai"} target="_blank" title="Instagram"><FaInstagram /></Link>
+                        <Link className="icon" to={"https://web.whatsapp.com/send?phone=254707251073"} target="_blank" title="WhatsApp"><FaWhatsapp /></Link>
+                        <Link className="icon" to={"tel:+254707251073"} target="_blank" title="Cell phone"><FaPhoneVolume /></Link>
+                        <Link className="icon" to={"mailto:ndungu.muigai01@gmail.com"} target="_blank" title="Email"><IoMailOutline /></Link>
                     </div>
                     <img src={Logo} alt="Logo" className="w-full mt-4" />
                 </div>
             </div>
-            <div className="text-center w-full max-w-4xl mx-auto">
+            <div className="text-center w-full max-w-4xl mx-auto mb-3">
                 <hr className="m-5" />
                 &copy; {year} Ndung'u Muigai - All rights reserved
             </div>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => 
 {
     return ( 
-        <nav className="bg-slate-600 text-white flex flex-row justify-end p-5 text-lg space-x-4 border-red-400 w-full fixed top-0">
+        <nav className="bg-slate-600 text-white flex flex-row p-5 text-md space-x-4 border-red-400 w-full fixed top-0 md:text-lg justify-end ">
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
             <Link to="/projects">Projects</Link>
