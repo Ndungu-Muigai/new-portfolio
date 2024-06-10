096053b2d8f252beb131323343073b24ca027b3f
Samuel Muigai <107753424+Ndungu-Muigai@users.noreply.github.com>
2024-06-10 15:46:54 +0300

    Made the home page occupy the available screen height

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,7 +5,7 @@ import Logo from '../assets/Images/Logo.png';
 
 const Home = () => {
     return (
-        <div className="flex justify-center items-center h-screen">
+        <div className="flex flex-col md:flex-row justify-center items-center h-screen">
             <div className="text-center text-white w-full md:w-1/2 md:mr-8">
                 <h1 className="text-3xl md:text-4xl font-bold mb-4">Hello there</h1>
                 <p className="text-lg md:text-xl mb-4">My name is Samuel Muigai</p>
@@ -28,7 +28,7 @@ const Home = () => {
                     <Link to="/contacts" className="button font-bold py-2 px-4 rounded hover:text-white hover:underline">Contact Me</Link>
                 </div>
             </div>
-            <div className="w-1/2 mr-8 hidden md:block">
+            <div className="w-full md:w-1/2 mr-8 hidden md:block">
                 <img src={Logo} alt="Samuel Ndung'u Muigai" className="w-full rounded-lg shadow-xl"/>
             </div>
         </div>
