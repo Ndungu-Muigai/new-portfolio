75404e71b33eadf97c487fe9f8cfd91af69d4add
Samuel Muigai <107753424+Ndungu-Muigai@users.noreply.github.com>
2024-06-10 16:09:14 +0300

    Changed the background color of the navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom"
 const Navbar = () => 
 {
     return (
-        <nav className=" bg-inherit border-b-2 flex flex-row p-5 text-base space-x-6 w-full fixed top-0 md:text-lg justify-end z-50">
+        <nav className=" bg-slate-800 border-b-2 flex flex-row p-5 text-base space-x-6 w-full fixed top-0 md:text-lg justify-end z-50">
             <NavLink to="/" className="nav-links">Home</NavLink>
             <NavLink to="/about" className="nav-links">About</NavLink>
             <NavLink to="/about#projects" className="nav-links">Projects</NavLink>
