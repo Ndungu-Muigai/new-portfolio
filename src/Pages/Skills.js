184fdcf36522cc5dbf239533472d98566ea00f2a
Samuel Muigai <107753424+Ndungu-Muigai@users.noreply.github.com>
2024-06-11 14:54:13 +0300

    Added language names below each icon and added background color to the icons

--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -12,27 +12,27 @@ const Skills = () =>
             <div className="flex justify-center items-center mt-2">
                 <div className="grid grid-cols-4 gap-14">
                     <div className="skills-cols">
-                        <FaHtml5 className="skills-icons bg-html"/>
+                        <FaHtml5 className="skills-icons text-html"/>
                         <p>HTML5</p>
                     </div>
                     <div className="skills-cols">
-                        <FaCss3Alt className="skills-icons bg-blue"/>
+                        <FaCss3Alt className="skills-icons text-blue"/>
                         <p>CSS3</p>
                     </div>
                     <div className="skills-cols">
-                        <FaBootstrap className="skills-icons bg-purple"/>
+                        <FaBootstrap className="skills-icons text-purple"/>
                         <p>Bootstrap</p>
                     </div>
                     <div className="skills-cols">
-                        <RiTailwindCssFill className="skills-icons bg-lightblue"/>
+                        <RiTailwindCssFill className="skills-icons text-lightblue"/>
                         <p>Tailwind</p>
                     </div>
                     <div className="skills-cols">
-                        <IoLogoJavascript className="skills-icons bg-yellow-400"/>
+                        <IoLogoJavascript className="skills-icons bg-yellow-400 text-black"/>
                         <p>JavaScript</p>
                     </div>
                     <div className="skills-cols">
-                        <FaReact className="skills-icons bg-lightblue"/>
+                        <FaReact className="skills-icons text-lightblue"/>
                         <p>React</p>
                     </div>
                     <div className="skills-cols">
@@ -40,15 +40,15 @@ const Skills = () =>
                         <p>Python</p>
                     </div>
                     <div className="skills-cols">
-                        <SiMysql className="skills-icons bg-blue"/>
+                        <SiMysql className="skills-icons text-blue"/>
                         <p>MySQL</p>
                     </div>
                     <div className="skills-cols">
-                        <BiLogoPostgresql className="skills-icons bg-blue"/>
+                        <BiLogoPostgresql className="skills-icons text-blue"/>
                         <p>PostgreSQl</p>
                     </div>
                     <div className="skills-cols">
-                        <FaGitAlt className="skills-icons bg-git"/>
+                        <FaGitAlt className="skills-icons text-git"/>
                         <p>Git</p>
                     </div>
                     <div className="skills-cols">
