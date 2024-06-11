bd59b6517f177254f79654310b52401208977068
Samuel Muigai <107753424+Ndungu-Muigai@users.noreply.github.com>
2024-06-11 11:57:16 +0300

    Styling th icons

--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -4,47 +4,24 @@ import { RiTailwindCssFill } from "react-icons/ri";
 import { SiMysql } from "react-icons/si";
 import { BiLogoPostgresql } from "react-icons/bi";
 
+
 const Skills = () => 
 {
     return ( 
         <>
             <h1>Skills</h1>
-            <div className="flex flex-row justify-center items-center flex-wrap w-1/2">
-                <div className="grid grid-cols-4 gap-4">
-                    <div className="skills-icons border p-2">
-                        <FaHtml5 />
-                    </div>
-                    <div className="skills-icons border p-2">
-                        <FaCss3Alt />
-                    </div>
-                    <div className="skills-icons border p-2">
-                        <FaReact />
-                    </div>
-                    <div className="skills-icons border p-2">
-                        <FaPython />
-                    </div>
-                    <div className="skills-icons border p-2">
-                        <FaBootstrap />
-                    </div>
-                    <div className="skills-icons border p-2">
-                        <IoLogoJavascript />
-                    </div>
-                    <div className="skills-icons border p-2">
-                        <RiTailwindCssFill />
-                    </div>
-                    <div className="skills-icons border p-2">
-                        <SiMysql />
-                    </div>
-                    <div className="skills-icons border p-2">
-                        <BiLogoPostgresql />
-                    </div>
-                    <div className="skills-icons border p-2">
-                        <FaGitAlt />
-                    </div>
-                    <div className="skills-icons border p-2">
-                        <FaGithub />
-                    </div>
-                </div>
+            <div className="flex flex-row justify-center items-center flex-wrap space-x-6">
+                <FaHtml5 className="skills-icons"/>
+                <FaCss3Alt className="skills-icons"/>
+                <FaBootstrap className="skills-icons"/>
+                <RiTailwindCssFill className="skills-icons"/>
+                <IoLogoJavascript className="skills-icons"/>
+                <FaReact className="skills-icons"/>
+                <FaPython className="skills-icons"/>
+                <SiMysql className="skills-icons"/>
+                <BiLogoPostgresql className="skills-icons"/>
+                <FaGitAlt className="skills-icons"/>
+                <FaGithub className="skills-icons"/>
             </div>
         </>
      );
