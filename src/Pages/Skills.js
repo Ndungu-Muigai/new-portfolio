078bfcb606468699daf5e66f5c2b98bbd996b1d0
Samuel Muigai <107753424+Ndungu-Muigai@users.noreply.github.com>
2024-06-11 10:45:43 +0300

    Added the skills icons

--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -1,8 +1,28 @@
+import { FaHtml5, FaCss3Alt, FaReact, FaPython, FaBootstrap, FaGitAlt, FaGithub } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { SiMysql } from "react-icons/si";
+import { BiLogoPostgresql } from "react-icons/bi";
+
+
 const Skills = () => 
 {
     return ( 
         <>
             <h1>Skills</h1>
+            <div className="flex flex-row">
+                <FaHtml5/>
+                <FaCss3Alt/>
+                <FaBootstrap/>
+                <RiTailwindCssFill/>
+                <IoLogoJavascript/>
+                <FaReact/>
+                <FaPython/>
+                <SiMysql/>
+                <BiLogoPostgresql/>
+                <FaGitAlt/>
+                <FaGithub/>
+            </div>
         </>
      );
 }
