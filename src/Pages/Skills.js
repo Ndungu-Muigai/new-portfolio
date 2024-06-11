e0ed77b4313a5e5ce35f2a2c173fc84a7018a292
Samuel Muigai <107753424+Ndungu-Muigai@users.noreply.github.com>
2024-06-11 11:27:44 +0300

    Styling the skills section icons

--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -10,7 +10,7 @@ const Skills = () =>
     return ( 
         <>
             <h1>Skills</h1>
-            <div className="grid grid-cols-4 gap-2 p-4 w-1/2 justify-center items-center">
+            <div className="flex flex-row p-4 w-1/2 justify-center items-center">
                 <FaHtml5 className="skills-icons"/>
                 <FaCss3Alt className="skills-icons"/>
                 <FaBootstrap className="skills-icons"/>
