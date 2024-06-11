e5ce6ad4a1ced9d324ce0e53a62e89e200c10e61
Samuel Muigai <107753424+Ndungu-Muigai@users.noreply.github.com>
2024-06-11 10:49:55 +0300

    Styling the skills icons

--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -10,7 +10,7 @@ const Skills = () =>
     return ( 
         <>
             <h1>Skills</h1>
-            <div className="grid grid-columns-4">
+            <div className="grid grid-cols-4 gap-4">
                 <FaHtml5/>
                 <FaCss3Alt/>
                 <FaBootstrap/>
