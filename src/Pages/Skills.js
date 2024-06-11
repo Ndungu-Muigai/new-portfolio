04504139e9cfe8adcb27ccd34b704518aa66b064
Samuel Muigai <107753424+Ndungu-Muigai@users.noreply.github.com>
2024-06-11 11:53:01 +0300

    Styling the icons

--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -10,35 +10,35 @@ const Skills = () =>
     return ( 
         <>
             <h1>Skills</h1>
-            <div className="flex flex-row justify-center items-center flex-wrap space-x-6">
-                <div>
+            <div className="flex flex-row justify-center items-center flex-wrap w-1/2">
+                <div className="space-x-6">
                     <FaHtml5 className="skills-icons"/>
                 </div>
-                <div>
+                <div className="space-x-6">
                     <FaBootstrap className="skills-icons"/>
                 </div>
-                <div>
+                <div className="space-x-6">
                     <RiTailwindCssFill className="skills-icons"/>
                 </div>
-                <div>
+                <div className="space-x-6">
                     <IoLogoJavascript className="skills-icons"/>
                 </div>
-                <div>
+                <div className="space-x-6">
                     <FaReact className="skills-icons"/>
                 </div>
-                <div>
+                <div className="space-x-6">
                     <FaPython className="skills-icons"/>
                 </div>
-                <div>
+                <div className="space-x-6">
                     <SiMysql className="skills-icons"/>
                 </div>
-                <div>
+                <div className="space-x-6">
                     <BiLogoPostgresql className="skills-icons"/>
                 </div>
-                <div>
+                <div className="space-x-6">
                     <FaGitAlt className="skills-icons"/>
                 </div>
-                <div>
+                <div className="space-x-6">
                     <FaGithub className="skills-icons"/>               
                 </div>
             </div>
