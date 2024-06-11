1a5268afa533eed026080fc4a062afaa5f617c07
Samuel Muigai <107753424+Ndungu-Muigai@users.noreply.github.com>
2024-06-11 12:16:11 +0300

    Styling the icons

--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -10,7 +10,7 @@ const Skills = () =>
         <>
             <h1>Skills</h1>
             <div className="flex justify-center items-center">
-                <div className="grid grid-cols-4 gap-8">
+                <div className="grid grid-cols-4 gap-12">
                     <FaHtml5 className="skills-icons"/>
                     <FaCss3Alt className="skills-icons"/>
                     <FaBootstrap className="skills-icons"/>
