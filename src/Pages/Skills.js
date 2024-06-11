0da94bee0f07b88621c324514c0cd8cc65e8914b
Samuel Muigai <107753424+Ndungu-Muigai@users.noreply.github.com>
2024-06-11 11:02:16 +0300

    Styling the skills section icons

--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -11,17 +11,17 @@ const Skills = () =>
         <>
             <h1>Skills</h1>
             <div className="grid grid-cols-4 gap-1.5 p-3">
-                <FaHtml5/>
-                <FaCss3Alt/>
-                <FaBootstrap/>
-                <RiTailwindCssFill/>
-                <IoLogoJavascript/>
-                <FaReact/>
-                <FaPython/>
-                <SiMysql/>
-                <BiLogoPostgresql/>
-                <FaGitAlt/>
-                <FaGithub/>
+                <FaHtml5 className="skills-icons"/>
+                <FaCss3Alt className="skills-icons"/>
+                <FaBootstrap className="skills-icons"/>
+                <RiTailwindCssFill className="skills-icons"/>
+                <IoLogoJavascript className="skills-icons"/>
+                <FaReact className="skills-icons"/>
+                <FaPython className="skills-icons"/>
+                <SiMysql className="skills-icons"/>
+                <BiLogoPostgresql className="skills-icons"/>
+                <FaGitAlt className="skills-icons"/>
+                <FaGithub className="skills-icons"/>
             </div>
         </>
      );
