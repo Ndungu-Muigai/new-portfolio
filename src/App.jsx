3a376e9163ed8250fac2604edd96678fed0a9855
Ndungu-Muigai <ndungu.muigai01@gmail.com>
2025-03-08 13:26:32 +0300

    Made the right side of the page scrollable and the left one sticky

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,34 @@ function App() {
 
   return (
     <div className='dark-navy min-h-screen text-slate-400'>
-      <Intro/>
+      <div className='lg:flex lg:justify-between lg:gap-4'>
+        <header className="p-6 md:p-8 lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-[48%] lg:flex-col lg:justify-between lg:py-16">
+          <Intro/>
+        </header>
+        <div className='pt-10 lg:w-[52%] lg:py-16'>
+          This is rhe right hand side section
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam omnis impedit tempore corporis doloremque, corrupti qui ipsa ea itaque voluptas sunt pariatur iure voluptates vitae quaerat, laborum iusto ratione officia.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto cupiditate error minus neque inventore commodi. Reprehenderit cumque, laudantium magni ad eum iste iusto sapiente explicabo vitae, laboriosam soluta, odio voluptates?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, quod. Nisi in sunt eum voluptas enim amet fugit eveniet. Labore, suscipit. Soluta, odio. Enim ut aperiam veritatis dignissimos, provident maxime!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel magni accusantium nam, provident laborum reprehenderit voluptas totam, aliquam non minima reiciendis nisi nemo asperiores at laudantium culpa quia magnam! Iure.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolorum facere obcaecati architecto ad reiciendis voluptatum ipsam eos et aspernatur. Animi cupiditate modi tenetur nemo facilis deleniti magnam deserunt quas.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat dolorem ratione labore aut ipsa, nam praesentium. Earum nostrum dignissimos unde sequi praesentium repellat adipisci, pariatur velit, aliquid molestiae vel dolores!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis facere est dignissimos non amet tempora placeat nemo, sed accusantium nesciunt dicta, laudantium id, consectetur rerum quod quae alias? Saepe, commodi.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur consequatur tenetur aperiam molestias explicabo a doloremque veritatis, placeat quibusdam ratione optio commodi eum mollitia! Harum amet corrupti ex laborum quia.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum ad earum libero ducimus voluptas. Deleniti vitae ducimus porro. Expedita culpa itaque non officia soluta sequi eligendi optio voluptatibus assumenda. Beatae!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, aspernatur dolore ipsum illum quia ea! Dignissimos obcaecati similique amet, numquam ducimus ea illum, quas illo nam veniam, explicabo excepturi est?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ab provident ea assumenda quo molestiae aliquam et numquam, consequuntur ipsa laboriosam quidem nesciunt commodi pariatur eveniet adipisci unde eos harum.
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus excepturi at quibusdam laboriosam cupiditate in dolor reiciendis expedita unde explicabo facere, libero perspiciatis vel illo! Ipsum in maxime voluptatibus ad!
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi aliquam officia, laudantium nulla voluptatibus sint cum placeat dignissimos, qui accusamus beatae doloremque aut distinctio. Reprehenderit laudantium aliquid officiis nesciunt eveniet.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus unde debitis repellat aut deserunt consequatur ducimus, nobis quos possimus illo natus et inventore minima beatae harum nihil tempora, similique nemo?
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates id tempora illum nisi corporis, nam dolore architecto laudantium soluta earum inventore aspernatur sapiente? Repellat at dolorem nemo optio nihil corporis.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor hic incidunt, reprehenderit debitis nobis, recusandae ut culpa, harum eligendi ducimus corrupti architecto delectus voluptatibus? Porro reprehenderit incidunt totam culpa asperiores?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit illo harum aliquam dolorum maxime vel dolore, eveniet eum aliquid labore pariatur. Culpa debitis reprehenderit suscipit nostrum! Nobis accusantium consequuntur voluptatum!
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias facilis iure, repellendus esse doloribus, odit est sit, quam dolorem consequatur sed quis provident molestiae. Rem consectetur perspiciatis alias hic neque!
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt illum assumenda reiciendis sit. Sed, consequatur quos! Dicta quia autem sit corporis quisquam obcaecati odio, qui omnis, velit rem sunt pariatur!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium ab dolor quae mollitia placeat expedita illum provident maxime exercitationem in deleniti quod, labore, officiis, iste hic adipisci? Harum, iste eum.
+        </div>
+      </div>
     </div>
   )
 }
